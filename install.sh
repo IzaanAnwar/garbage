f75924910837b230f9137cbbfa7291d36adc413a
IzaanAnwar <mdizaan67@gmail.com>
2023-10-12 16:16:36 +0530

    update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Build The Project
-Cargo build --release
+cargo build --release
 
 #Install the built binary to usr/local/bin
 echo "Installing in usr/local/bin"
 sudo cp ./target/release/garbage /usr/local/bin
-echo"Installed Successfully"
+echo "Installed Successfully"
 
